8d1e4ac6ca59a5f78c85c3e1bd1e5c73d0124052
github username <github email address>
2019-08-03 22:27:11 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main.remove();
+
+// has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+let newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Lawson is the champion';
